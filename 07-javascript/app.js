d099df9c2ad6f10faac6efea143a63bc9e187fe1
Andy <jiancong.liu@outlook.com>
2022-07-04 18:57:49 +0800

    outsourcing js code as a file

---
+++ 07-javascript/app.js
@@ -0,0 +1,11 @@
+let age = 32;
+let userName = "Andy";
+let hobbies = ["Sprots", "Cooking", "Reading"];
+// a object
+let job = {
+  title: "Software Engineer",
+  place: "Berlin",
+  salary: 50000,
+};
+
+
