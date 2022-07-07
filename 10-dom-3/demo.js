75d4e537dd435cc6609a70a8487e0b4ae47d7ae9
Andy <jiancong.liu@outlook.com>
2022-07-07 08:41:32 +0800

    add js exercise

--- 10-dom-3/demo.js
+++ 10-dom-3/demo.js
@@ -1,5 +1,6 @@
-const productNameInputElement = document.getElementById('product-name');
-const remainingCharsElement = document.getElementById('remaining-chars');
+//A JS demo to show characters limitation of an input field.
+const productNameInputElement = document.getElementById("product-name");
+const remainingCharsElement = document.getElementById("remaining-chars");
 
 // console.dir(productNameInputElement);
 
@@ -7,11 +8,11 @@ const maxAllowedChars = productNameInputElement.maxLength;
 
 function updateRemainingCharacters(event) {
   const enteredText = event.target.value;
-  const enteredTextLength = enteredText.length; 
+  const enteredTextLength = enteredText.length;
 
   const remainingCharacters = maxAllowedChars - enteredTextLength;
 
   remainingCharsElement.textContent = remainingCharacters;
 }
 
-productNameInputElement.addEventListener('input', updateRemainingCharacters);
+productNameInputElement.addEventListener("input", updateRemainingCharacters);
