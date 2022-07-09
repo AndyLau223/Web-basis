eeb58fa284549ab8e08df0d00d74aa58502027c2
Andy <jiancong.liu@outlook.com>
2022-07-09 14:08:45 +0800

    add express.js

--- 15-nodejs-01/app.js
+++ 15-nodejs-01/app.js
@@ -1,14 +1,21 @@
-const http = require("http");
+const express = require('express');
 
-function handleRequest(request, response) {
-  response.statusCode = 200;
-  response.end("<h1>Hello Wrold!</h1>");
-}
+const app = express();
 
-http.createServer(handleRequest).listen(3000);
+app.use(express.urlencoded({extended: false}));
 
-// create a server and listening to a specific port.
-// server.listen(3000);
+app.get('/currenttime', function(req, res) {
+  res.send('<h1>' + new Date().toISOString() + '</h1>');
+}); // localhost:3000/currenttime
 
-// amazon.com ==> Send a request to Amazon's server
-// amazon.com:443  or 80===> 80 and 443 are the default ports exposed by all web servers to handle incoming traffic(requests).
+app.get('/', function(req, res) {
+  res.send('<form action="/store-user" method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>');
+}); // localhost:3000/
+
+app.post('/store-user', function(req, res) {
+  const userName = req.body.username;
+  console.log(userName);
+  res.send('<h1>Username stored!</h1>');
+});
+
+app.listen(3000)
