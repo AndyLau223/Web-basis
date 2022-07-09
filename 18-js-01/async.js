09295516d85ecae754a3cdac4a796e4f557d7f19
Andy <jiancong.liu@outlook.com>
2022-07-09 21:47:04 +0800

    introduce promises

--- 18-js-01/async.js
+++ 18-js-01/async.js
@@ -1,9 +1,25 @@
-const fs = require('fs');
+const fs = require('fs/promises'); // give us promises version of file system method
 
-function readFile() {
-  const fileData = fs.readFile('data.txt', function (error, fileData) {
-    console.log('File parsigng done! ' + fileData.toString());
-  });
+async function readFile() {
+  // const fileData = fs.readFile('data.txt', function (error, fileData) {
+  //   console.log('File parsigng done! ' + fileData.toString());
+
+  //   // start another async task that sends the data to a database
+  // });
+
+  // fs.readFile('data.txt')
+  //   .then(function (fileData) {
+  //     console.log('File parsigng done! ' + fileData.toString());
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
+
+  try {
+    const fileData = await fs.readFile('data.txt');
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 readFile();
